f0a82ee41ab58a46cde76ce381f69faf408266df
Shahar Kehila.Tech <kehila@tech>
2024-02-16 09:45:40 +0200

    react router

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState, useEffect } from "react";
+import { BrowserRouter, Routes, Route, useParams } from "react-router-dom";
 
 function ViewPage(props) {
   return (
@@ -37,10 +38,12 @@ function EditPage(props) {
   );
 }
 
-function App() {
-  const [page, setPage] = useState('ראשי');
+function Page() {
+  const { id } = useParams();
   const [editMode, setEditMode] = useState(false);
   const [content, setContent] = useState('טוען...');
+
+  const page = id ? id : 'ראשי';
 
   useEffect(() => {
     const async_impl = async () => {
@@ -67,7 +70,19 @@ function App() {
       <button onClick={toggleEditMode}>
         {editMode ? "תצוגת עמוד" : "עריכת עמוד"}
       </button>
+      <p><a href="/">חזרה לעמוד הראשי</a></p>
     </div>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Page />} />
+        <Route path="/:id" element={<Page />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
