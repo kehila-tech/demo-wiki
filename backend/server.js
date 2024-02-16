9de9ec640c6a9cd23aa7ae4f81af62c26f14866c
Shahar Kehila.Tech <kehila@tech>
2024-02-16 09:02:24 +0200

    toggle edit mode

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true})); 
+
 const port = 4000;
 
 app.get('/api/hello', async (req, res) => {
   res.send('שלום עולם');
+})
+
+const pages = new Map();
+pages.set('עמוד', 'זהו תוכן של עמוד');
+
+app.get('/api/page/get/:id', async (req, res) => {
+  const content = pages.get(req.params.id);
+  if (content) {
+    res.send(content);
+  } else {
+    res.send("עמוד לא נמצא");
+  }
+})
+
+app.post('/api/page/set/:id', async (req, res) => {
+  pages.set(req.params.id, req.body.content);
 })
 
 app.listen(port, () => {
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,21 +1,43 @@
 import './App.css';
 import { useState, useEffect } from "react";
 
-function App() {
+function ViewPage(props) {
   const [response, setResponse] = useState('טוען...');
 
   useEffect(() => {
     const async_impl = async () => {
-      const tmp_response = await fetch('/api/hello');
+      const tmp_response = await fetch(
+        `/api/page/get/${encodeURIComponent(props.page)}`
+      );
       setResponse(await tmp_response.text())
     };
     async_impl();
   });
 
   return (
-    <div className="App">
-      <h1>ברוכים הבאים</h1>
+    <>
+      <h1>{props.page}</h1>
       <p>{response}</p>
+    </>
+  );
+}
+
+function EditPage(props) {
+  return <p>עריכת עמוד {props.page}</p>;
+}
+
+function App() {
+  const [page, setPage] = useState('עמוד');
+  const [editMode, setEditMode] = useState(false);
+
+  const toggleEditMode = () => {
+    setEditMode(!editMode);
+  };
+
+  return (
+    <div className="App">
+      {editMode ? <EditPage page={page} /> : <ViewPage page={page} />}
+      <button onClick={toggleEditMode}>עריכת עמוד</button>
     </div>
   );
 }
