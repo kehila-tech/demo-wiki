46b94b7eb1b2db6a9ce60c704c618c43aed5cba1
Shahar Kehila.Tech <kehila@tech>
2024-02-16 08:39:33 +0200

    frontend communicates with backend

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const port = 4000;
 
 app.get('/api/hello', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World!');
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 })
--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    const async_impl = async () => {
+      const tmp_response = await fetch('/api/hello');
+      setResponse(await tmp_response.text())
+    };
+    async_impl();
+  });
+
   return (
     <div className="App">
       <h1>ברוכים הבאים</h1>
+      <p>{response}</p>
     </div>
   );
 }
